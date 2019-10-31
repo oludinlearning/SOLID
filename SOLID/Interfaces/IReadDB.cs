0380c8b6c486ce5459ae678129225bc991533d25
Nikdg <blackfire5924@gmail.com>
2019-10-31 23:26:53 +0500

    БД1.1

---
+++ SOLID/Interfaces/IReadDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Interfaces
+{
+    public interface IReadDB
+    {
+        public List<string> ReadDBs(int use, int fac, int spec, int group, int student);
+    }
+}
---
+++ SOLID/Models/ReadDB.cs
@@ -0,0 +1,19 @@
+﻿using SOLID.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.Models
+{
+    class ReadDB:IReadDB
+    {
+        List<string> dataBase;
+        public List<string> ReadDBs(int use, int fac, int spec, int group, int student)
+        {
+
+            return dataBase;
+        }
+    }
+}
